3ab3bb09968a6df41801f8ee54a3e82d95362068
Andre Henry <ahenry@thinkthroughmath.com>
2016-04-26 13:18:25 -0400

    testing

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -804,9 +804,9 @@ params = CGI.parse(uri.query || "")
 
   def run_assets_precompile_rake_task
     instrument 'ruby.run_assets_precompile_rake_task' do
-
-      precompile = rake.task("assets:precompile")
+      
       sleep(100000000)
+      precompile = rake.task("assets:precompile")
       return true unless precompile.is_defined?
 
       topic "Precompiling assets"
